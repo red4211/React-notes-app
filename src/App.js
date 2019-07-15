82be83effb67890e5882bc6e9d02d733a7aee57f
red42 <mperepelitsa421@gmail.com>
2019-07-15 14:15:01 +0300

    changes 1

--- src/App.js
+++ src/App.js
@@ -1,98 +1,120 @@
-import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
 
-class InputField extends Component{
-  render(){
-    return(
-        <input type='text' className="inpField" onKeyPress={this.props.testProp} />
-      )
-  }
+class InputField extends Component {
+    render() {
+        return (
+            <input type='text' className="inpField" placeholder="Enter your note here" onKeyPress={this.props.testProp} />
+        )
+    }
 }
 
-class NotesList extends Component{
-  render(){
-    var list = this.props.list;
-    var buildList = list.map((current, index)=>{
-      if(current.editable==true){
-        return <li key={index} onClick={()=>{this.props.editNote(index)}}> 
-        <input type='text' className='editInp' defaultValue={current.text} autoFocus onChange={(e)=>this.props.noteChange(index,e)} >
-        </input><div className="delete" onClick={this.props.evHandler} parentkey={index}>x</div></li>
-      }else{
-        return <li key={index} onClick={()=>{this.props.editNote(index)}}>{current.text} <div className="delete" onClick={this.props.evHandler} parentkey={index}>x</div></li>
+class NotesList extends Component {
+    render() {
+        var list = this.props.list;
+        var buildList = list.map((current, index) => {
+            if (current.editable == true) { //list item contents depend on true/false flag in state
+                return <li key={index}> 
+              <textarea className='editInp' defaultValue={current.text} autoFocus onKeyPress={(e)=>this.props.noteChange(index,e)} onBlur={(e)=>{ this.props.testEvt(index, e)}} ></textarea>
+               <div className="delete" parentkey={index} onClick={this.props.evHandler}>x</div> </li>
+            } else {
+                return <li key={index}><div onClick={()=>{this.props.editNote(index)}}>{current.text}</div> <div className="delete" parentkey={index} onClick={this.props.evHandler}>x</div></li>
+            }
+        });
+
+        return (
+            <ul className="notes-list">
+              {buildList}
+            </ul>
+        )
+    }
+}
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.deleteNote = this.deleteNote.bind(this);
+        this.editNotes = this.editNotes.bind(this);
+
+        this.notesChange = this.notesChange.bind(this);
+        this.blurEvt = this.blurEvt.bind(this);
+
+        this.state = {
+            notesText: []
+        }
+    }
+
+    componentDidMount() {
+        try {
+            const notesJSON = localStorage.getItem("notes");
+            if (notesJSON) {
+                const notesText = JSON.parse(notesJSON);
+                this.setState({notesText: notesText });
+            }
+        } catch (e) {
+           console.warn('JSON parse exception' + e);
+        }
       }
 
-      
-    });
+        handleKeyPress(e) {
+            if (e.key === 'Enter') {
+                let copyArr = this.state.notesText.slice();
+                copyArr.push({ text: e.target.value, editable: false });
+                e.target.value = '';
+                this.setState({ notesText: copyArr });
+                /*localStorage*/
+                localStorage.setItem("notes", JSON.stringify(copyArr));
+            }
+        }
 
-    return(
-        <ul className="notes-list">
-          {buildList}
-        </ul>
-      )
-  }
-}
+        deleteNote(e) {
+            let copy = this.state.notesText.slice();
+            let index = e.target.getAttribute("parentkey");
 
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.editNotes = this.editNotes.bind(this);
+            copy.splice(index, 1);
+            this.setState({ notesText: copy });
+            localStorage.setItem("notes", JSON.stringify(copy)); //dont forget to change localstorage
+        }
 
-    this.notesChange = this.notesChange.bind(this);
+        editNotes(num) {
+            let copy = this.state.notesText.slice();
+            copy[num].editable = true;
+            //change editable flag, renders input on click
 
-    this.state = {
-      notesText: []
+            this.setState({ notesText: copy });
+        }
+
+        notesChange(index, e) { //added extra argument w event
+            if (e.key === 'Enter') {
+
+                let copy = this.state.notesText.slice();
+                copy[index].text = e.target.value;
+                copy[index].editable = false;
+
+                this.setState({ notesText: copy });
+                localStorage.setItem("notes", JSON.stringify(copy));
+            }
+
+        }
+
+        blurEvt(index, e) {
+            let copy = this.state.notesText.slice();
+            copy[index].text = e.target.value;
+            copy[index].editable = false;
+
+            this.setState({ notesText: copy });
+            localStorage.setItem("notes", JSON.stringify(copy));
+        }
+
+        render() {
+            return (
+                <div className="App">
+                <p className="title" >Notes</p>
+                <InputField testProp={this.handleKeyPress} />
+                <NotesList list = {this.state.notesText} evHandler={this.deleteNote} editNote={this.editNotes} noteChange={this.notesChange} testEvt={this.blurEvt} />
+              </div>
+            )
+        }
     }
-  }
-  handleKeyPress(e){
-    if(e.key==='Enter'){
-      let copyArr = this.state.notesText.slice();
-      copyArr.push({text:e.target.value, editable:false});
-      e.target.value = '';
-      this.setState({notesText: copyArr});
-    }
-  }
-
-  handleClick(e){
-    let copy = this.state.notesText.slice();
-    let index = e.target.getAttribute("parentkey");
-
-    copy.splice(index, 1);
-    this.setState({notesText: copy})
-    }
-
-  editNotes(num){
-    let copy = this.state.notesText.slice();
-    copy[num].editable = true;
-    //rerenders list element as input
-
-    this.setState({notesText: copy});
-  }
-
-  notesChange(index, e){//added extra argument w event
-    console.log(e.target.value);
-    console.log(e.key);
-    /*if(e.key==='Enter'){
-
-    let copy = this.state.notesText.slice();
-    copy[index].text = e.target.value;
-    copy[index].editable = false;
-
-    this.setState({notesText: copy});
-    }*/
-
-  }
-
-  render(){
-    return (
-    <div className="App">
-      <InputField testProp={this.handleKeyPress} />
-      <NotesList list = {this.state.notesText} evHandler={this.handleClick} editNote={this.editNotes} noteChange={this.notesChange} />
-    </div>
-  )
-  }
-}
-
-export default App;
+    export default App;
